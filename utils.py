aaa072dfa7e38852dbccb6b420c52a17e2034456
user <user>
2019-04-24 16:25:43 +0000

    count files to check image counts in folders

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 from toolz import pipe as p
 
 def isImage(f):
@@ -12,3 +13,12 @@ def isOriginal(f):
 def unzip(tuple_arr):
     return p(zip(*tuple_arr), list)
 
+
+def fileCount(parent_dir):
+    add_d = lambda f: os.path.join(parent_dir, f)
+    over_children = lambda fn: [c for c in os.listdir(parent_dir) if p(c, add_d, fn)]
+
+    n_fs = p(os.path.isfile, over_children, len)
+    ds = over_children(os.path.isdir)
+
+    return n_fs + sum(p(d, add_d, fileCount) for d in ds)
