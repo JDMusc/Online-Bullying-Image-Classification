04863ad7d2a34ad6f7a80b54aa1667a3015fe40e
JDMUSC <user>
2019-05-02 05:08:20 -0400

    code to test final script

---
+++ testfinal.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import sys
+
+import torch
+
+import preprocessing as pp
+import vggTransfer
+
+f_name = str(sys.argv[1])
+
+device = torch.device("cuda")
+
+vgg = vggTransfer.loadVgg(n_classes = 10)
+if torch.cuda.is_available():
+    vgg = vgg.to(device)
+
+vgg.load_state_dict(torch.load('attempts/vgg_transfer_full/models/model_11.pt'))
+vgg.eval()
+
+tform = pp.createDataTransforms()['val']
+img = tform(Image.open(f_name))
+if torch.cuda.is_available():
+    img = img.to(device)
+
+img.unsqueeze_(0)
+
+classes = ['gossiping',
+    'isolation',
+    'laughing',
+    'nonbullying',
+    'pullinghair',
+    'punching',
+    'quarrel',
+    'slapping',
+    'stabbing',
+    'strangle']
+
+_, pred = torch.max(vgg(img), 1)
+print(classes[pred])
